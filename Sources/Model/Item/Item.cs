0a9838250d3c7d51b328a6597938e224c2197ffc
gnoudeath <87683653+gnoudeath@users.noreply.github.com>
2023-03-27 18:57:37 +0700

    first commit

---
+++ Sources/Model/Item/Item.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NRO_Server.Model.ModelBase;
+
+namespace NRO_Server.Model.Item
+{
+    public class Item : ItemBase, IDisposable
+    {
+        internal int itemId;
+
+        public int IndexUI { get; set; }
+        public int SaleCoin { get; set; } = 1;
+        public long BuyPotential { get; set; }
+        public object Index { get; internal set; }
+
+        public Item() : base()
+        {
+            
+        }
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+    }
+}
